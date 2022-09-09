cb0808fdab5879507cb975e7c8619e6fac85027d
Mingyuan Xie <mingyuanxie@MingyuandeMacBook-Pro.local>
2022-09-08 20:44:17 -0400

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>Mingyuan Xie</Text>
+      <Text style={styles.textStyle}>301275467</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle:{
+    fontSize: 30
+  }
 });
